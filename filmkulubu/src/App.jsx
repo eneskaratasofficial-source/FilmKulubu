7b05eaa319c6078557b5e36d52e927b98b917c9a
eneskaratasofficial-source <eneskaratasofficial@gmail.com>
2025-11-02 00:07:08 +0300

    Update for correction
    Removed unnecessary  component

--- filmkulubu/src/App.jsx
+++ filmkulubu/src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
           <Route path="/" element={<Anasayfa />} /> 
           <Route path="/detay/:id" element={<DiziDetay />} /> 
         </Routes>
-        <Altbilgi olusturanAd="[ENES KARATAŞ-2321032069]" /> 
+      ---  //FOOTER ALANINDA BURASI VAR BU SATIRA GEREK YOK <Altbilgi olusturanAd="[ENES KARATAŞ-2321032069]" /> ----
       </Router>
     </DurumYoneticisi>
   );
